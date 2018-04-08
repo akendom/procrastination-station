14cc945d6765ffe5dc9de8549b8d58ce8465ec46
akendom <34591970+akendom@users.noreply.github.com>
2018-04-07 20:36:57 -0400

    initial commit

---
+++ populate.js
@@ -0,0 +1,61 @@
+var randXAdd = Math.floor(Math.random()*50)+1;
+var randYAdd = Math.floor(Math.random()*50)+1;
+var resultAdd = randXAdd + randYAdd;
+var randXMult = Math.floor(Math.random()*10)+1;
+var randYMult = Math.floor(Math.random()*15)+1;
+var resultMult = randXMult * randYMult;
+var randXExp = Math.floor(Math.random()*10)+1;
+var randNExp = Math.floor(Math.random()*5);
+var resultExp = Math.pow(randXExp, randNExp);
+var timeLeft = 30;
+var elem = document.getElementById('clocky');
+var timerId = setInterval(countdown, 1000);
+
+
+function onLoad(){
+  document.getElementById("question1").innerHTML = "What is the result of " + randXMult + " times " + randYMult+ "?";
+  document.getElementById("question2").innerHTML = "What is the result of " + randXAdd + " + " + randYAdd+ "?";
+  document.getElementById("question3").innerHTML = "What is the result of " + randXExp + " to the power of " + randNExp+ "?";
+
+
+}
+
+function countdown() {
+  if (timeLeft == 0) {
+    
+randXAdd = Math.floor(Math.random()*50)+1;
+randYAdd = Math.floor(Math.random()*50)+1;
+resultAdd = randXAdd + randYAdd;
+randXMult = Math.floor(Math.random()*10)+1;
+randYMult = Math.floor(Math.random()*15)+1;
+resultMult = randXMult * randYMult;
+randXExp = Math.floor(Math.random()*10)+1;
+randNExp = Math.floor(Math.random()*2);
+resultExp = Math.pow(randXExp, randNExp);
+    
+    onLoad();
+    timeLeft = 30;
+  } else {
+    elem.innerHTML = timeLeft + ' seconds remaining';
+    timeLeft--;
+  }
+}
+
+function toFacebook() {
+  var userInput1 = document.getElementById('userInput1').value;
+  var userInput2 = document.getElementById('userInput2').value;
+  var userInput3 = document.getElementById('userInput3').value;
+  console.log(userInput1);
+  console.log(resultMult);
+  console.log(userInput2);
+  console.log(resultAdd);
+  console.log(userInput3);
+  console.log(resultExp);
+  if ((userInput1 == resultMult) && (userInput2 == resultAdd)&& (userInput3 == resultExp)){
+    window.location = "https://facebook.com";
+    alert("Have fun!")
+        }
+    else{
+      alert("WROOOONNNGG!");
+         }
+ }
